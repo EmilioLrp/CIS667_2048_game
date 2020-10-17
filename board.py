2a9751ecda3f1ecaf24b9402dd8af34a8ff9fc1d
EmilioLIU <14251493@life.hkbu.edu.hk>
2020-10-17 00:58:55 -0400

    feat: done construction of extracting lines and merging line ops, which completes the construction of update_board function, need test

--- board.py
+++ board.py
@@ -8,6 +8,12 @@ class Board:
         self._game_board = np.zeros((self._board_size, self._board_size), dtype=int)
         self._init_game()
 
+    def get_board(self):
+        return self._game_board
+
+    def get_size(self):
+        return self._board_size
+
     def _init_game(self):
         # generate 2 tiles with value 2 at random position
         self._generate_rand_tile()
@@ -18,5 +24,34 @@ class Board:
         tile = random.sample(list(empty), 1)
         self._game_board[tile[0][0]][tile[0][1]] = 2
 
-    def merge_tile(self, line):
-        pass
+    def merge_tile(self, lines, merge_to_left=True):
+        """
+        This function process the lines that extracted according to the direction of user's movement
+        :param lines: list of line that extracted from the game board according to the user's movement
+        each element of line: pair of x, y coordinate of the tile
+        :param merge_to_left: controls the direction of merging
+        :return:
+        """
+        for line in lines:
+            if not merge_to_left:
+                line = np.flip(line, 0)
+            for i in range(len(line) - 1):
+                j = i + 1
+                start_tile = self._get_tile(line, i)
+                end_tile = self._get_tile(line, j)
+                if start_tile == end_tile:
+                    self._game_board[line[i][0], line[i][1]] = start_tile + end_tile
+                    self._merge_line(line, index=j)
+        self._generate_rand_tile()
+
+    def _merge_line(self, line, index):
+        for i in range(index, len(line-1)):
+            j = i + 1
+            # move each tile to the left of the line by 1
+            self._game_board[line[i][0], line[i][1]] = self._game_board[line[j][0], line[j][1]]
+        self._game_board[line[len(line)-1][0], line[len(line)-1][1]] = 0
+
+    def _get_tile(self, line, line_index):
+        x = line[line_index][0]
+        y = line[line_index][1]
+        return self._game_board[x,y]
--- game.py
+++ game.py
@@ -1,27 +1,85 @@
 from actions import Action
 from board import Board
+import numpy as np
 
 
 class Game:
     def __init__(self):
         self._game_board = Board()
+        self._board_indedxes = self._game_board_indexes(self._game_board.get_size())
+        self._goal = 2048
+
+    def _game_board_indexes(self, size):
+        board_index = np.ndarray(shape=(size, size), dtype=object)
+        for r in range(board_index.shape[0]):
+            for c in range(board_index.shape[1]):
+                board_index[r, c] = (r, c)
+        return board_index
 
     def display(self):
-        print(self._game_board)
+        print(self._game_board.get_board())
 
     def update_state(self, action):
-        if action == Action.left:
-            line = []
-            self._game_board.merge_tile(line=line)
-            pass
-        elif action == Action.right:
-            pass
+        """
+        First, extract lines according to user's input, within each line holds the coordinate of each tile
+        Then, move and merge tiles
+        :param action:
+        :return:
+        """
+        lines = []
+        if action == Action.left or action == Action.right:
+            lines = self._get_row_lines(indexes=self._board_indedxes)
+        elif action == Action.up or action == Action.down:
+            # a vertical operation, transpose the board, then call _get_row_lines
+            index_trans = np.transpose(self._board_indedxes)
+            lines = self._get_row_lines(indexes=index_trans)
+        elif action == Action.upLeft or action == Action.downLeft:
+            lines = self._get_diagonal_lines(indexes=self._board_indedxes)
+        else:
+            # a forward slash operation, flip the board vertically and call _get_diagonal_lines
+            index_flip = np.fliplr(self._board_indedxes)
+            lines = self._get_diagonal_lines(indexes=index_flip)
+        if action in [Action.up, Action.left, Action.upLeft, Action.upRight]:
+            self._game_board.merge_tile(lines=lines, merge_to_left=True)
+        else:
+            self._game_board.merge_tile(lines=lines, merge_to_left=False)
+
+        if action in [Action.left, Action.up, Action.upRight, Action.upLeft]:
+            self._game_board.merge_tile(lines=lines, merge_to_left=True)
+        else:
+            self._game_board.merge_tile(lines=lines, merge_to_left=False)
+
+    def _get_row_lines(self, indexes):
+        lines = []
+        # a horizontal operation, get coordinate of each tile from each row
+        for i in range(indexes.shape[0]):
+            lines.append(indexes[i, :])
+        return lines
+
+    def _get_diagonal_lines(self, indexes):
+        # get the diagonal of the board first
+        lines = [np.diagonal(indexes)]
+        for i in range(1, indexes.shape[0] - 2):
+            # shrink the board to the down left corner by 1 tile, then get its diagonal
+            lines.append(np.diagonal(indexes[i:, :(indexes.shape[1] - i)]))
+            # shrink the board to the up right corner by 1 tile, then get its diagonal
+            lines.append(np.diagonal(indexes[:(indexes.shape[1] - i), i:]))
+        return lines
 
     def valid_action(self, action):
-        pass
+        return False
 
+    def _valid_actions(self):
+        return 1
+
+    @property
     def game_over(self):
-
+        if self._valid_actions() == 0:
+            print("you loose!!!")
+            return True
+        if np.any(self._game_board.get_board() == self._goal):
+            print("you win !!!")
+            return True
         return False
 
 
@@ -29,11 +87,10 @@ if __name__ == '__main__':
     game = Game()
     move_count = 0
     game.display()
-    while True:
-        if game.game_over():
-            print("")
-            break
+    while not game.game_over:
         action = input("Please input a move: ")
         if not game.valid_action(action=action):
             continue
-            # @TODO update game board
+        # @TODO update game board
+        game.update_state(action=action)
+        game.display()
